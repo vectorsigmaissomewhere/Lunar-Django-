9a3f5de63533b3b9c1bc185fd2f7d54abf77430b
Vector Sigma <anishchy501625@gmail.com>
2024-07-01 17:03:01 -0700

    removed the model

--- lunar/myapp/models.py
+++ lunar/myapp/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 
-class MyModel(models.Model):
-    field1 = models.CharField(max_length=100)
-    field2 = models.IntegerField()
-
-    class Meta:
-        db_table = 'custom_table_name'
