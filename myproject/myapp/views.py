1da46f514c948ec94b25ea35f829dbd6e7381980
Vector Sigma <anishchy501625@gmail.com>
2024-07-03 07:45:57 -0700

    read update delete of blog has been made

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Blogs
 from django.contrib import messages
 
@@ -10,7 +10,42 @@ def create(request):
         image = request.FILES.get('image') 
         Blogs.objects.create(title=title, subtitle=subtitle, description=description, image=image)
         # sending a message to the create.html that the blog has been added
-        messages.add_message(request,messages.INFO, "Your blog has been created")
+        messages.success(request, "Your blog has been created")
         return redirect('create')
     
     return render(request, 'create.html')
+
+def read(request):
+    if request.method == "GET":
+        queryset = Blogs.objects.all()
+        context = {'blogs': queryset}
+        return render(request,'read.html',context)
+    
+    return render(request,'read.html')
+
+def delete(request, id):
+    blog = Blogs.objects.get(id=id) 
+    blog.delete()
+    messages.success(request, "Your blog has been deleted")
+    return redirect('read') 
+
+def update(request,id):
+    blog = Blogs.objects.get(id=id)
+    if request.method == "POST":
+        title = request.POST.get('title')
+        subtitle = request.POST.get('subtitle')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+        blog.title = title
+        blog.subtitle = subtitle
+        blog.description = description
+
+        if image:
+            blog.image = image
+        
+        blog.save()
+        messages.success(request, "Your blog has successfully updated")
+        return redirect('read')
+
+    context = {'blogs':blog}
+    return render(request, 'update.html', context)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -4,10 +4,16 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from myapp.views import create
+from myapp.views import read
+from myapp.views import delete
+from myapp.views import update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create/',create, name="create"),
+    path('read/',read,name="read"),
+    path('update/<int:id>', update, name='update'),
+    path('delete/<int:id>/', delete, name='delete'),
 ]
 
 if settings.DEBUG:
