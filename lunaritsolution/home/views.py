947f96360c50e4fd1e1cb91c13678e6bb088db16
Vector Sigma <anishchy501625@gmail.com>
2024-07-25 13:03:42 -0700

    lunar it solution

--- lunaritsolution/home/views.py
+++ lunaritsolution/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm # login form
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
 
 def home(request):
     return render(request,'home.html')
@@ -8,3 +11,33 @@ def course(request):
 
 def specificcourse(request):
     return render(request, 'specificcourse.html')
+
+def userdashboard(request):
+    return render(request, 'userdashboard.html')
+
+def enrolledcourse(request):
+    return render(request, 'enrolledcourse.html')
+
+def certificate(request):
+    return render(request, 'certificate.html')
+
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            fm = AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in successfully !!')
+                    return HttpResponseRedirect('/home/')
+        else:
+            fm = AuthenticationForm()
+        return render(request, 'userlogin.html',{'form':fm})
+    else:
+        return HttpResponseRedirect('/home/')
+
+
+
--- lunaritsolution/lunaritsolution/urls.py
+++ lunaritsolution/lunaritsolution/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from home.views import home, course, specificcourse
+from home.views import home, course, specificcourse, userdashboard, enrolledcourse, certificate, user_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home,name="home"),
     path('course/',course, name='course'),
-    path('specificcourse/',specificcourse, name='specificcourse')
+    path('specificcourse/',specificcourse, name='specificcourse'),
+    path('userdashboard/',userdashboard, name='userdashboard'),
+    path('enrolledcourse/',enrolledcourse, name='enrolledcourse'),
+    path('certificate/', certificate, name='certificate'),
+    path('login/',user_login, name='login'),
 ]
